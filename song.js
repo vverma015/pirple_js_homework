a82249d8bf89f0c2f958bb80cf464fbc66323e52
vv <ashish.joshi@dotsquares.com>
2019-01-03 16:25:16 +0530

    up song js

---
+++ song.js
@@ -0,0 +1,11 @@
+//variable durection
+var artist ="vijay"
+var genre ="clasic"
+var durection="300"
+var published="2000"
+var catagory="album"
+
+// console
+console.log(artist);
+console.log(genre);
+console.log(durection);
